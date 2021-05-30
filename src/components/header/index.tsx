408fdefe7e9adce4e0bee061347896b15538b796
Matheus Lucas <matheuslucas.ssilva@gmail.com>
2021-05-30 16:25:25 -0300

    Adicionar componente header

---
+++ src/components/header/index.tsx
@@ -0,0 +1,21 @@
+import ptBR from 'date-fns/locale/pt-BR';
+import format from 'date-fns/format';
+
+import styles from  './styles.module.scss';
+
+export default function Header() {
+
+    const currentDate = format(new Date(), 'EEEEEE, d MMMM', {
+        locale: ptBR,
+    });
+
+    return (
+        <header className={styles.headerContainer}>
+            <img src="/logo.svg" alt="PodCastr" />
+
+            <p> O melhor para você ouvir, sempre</p>
+
+            <span>{currentDate}</span>
+        </header>
+    )
+}
